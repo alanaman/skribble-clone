f87996f96d07875f6596d615157e5717e625f5fc
Kartik Dhawaniya <200050060@iitb.ac.in>
2021-10-19 12:30:30 +0530

    Working on room joining functionality

--- index.js
+++ index.js
@@ -10,15 +10,23 @@ var server = app.listen(3999, function(){
 app.use(express.static('public'));
 
 // Socket setup
-var io = socket(server);
+
+
+const io = require('socket.io')(server, {
+    cors: {
+      origin: '*',
+    }
+  });
+
+// const io = socket(server);
 
 //data declaration
 players=[];
-
+rooms = [];
 
 
 io.on('connection', function(socket){
-    console.log('connection to'+ socket.id);
+    console.log('connection to '+ socket.id);
     socket.on('new_player',function(data){
         if(players.includes(data.username)){
             io.to(socket.id).emit('validation',{success: false});
@@ -26,8 +34,38 @@ io.on('connection', function(socket){
         else{
             players.push(data.username);
             console.log("added "+data.username);
-            io.to(socket.id).emit('validation',{success: true});
+            io.to(socket.id).emit('validation',{success: true, rooms: rooms});
         }
     });
+    socket.on('new_room',function(data){
+        if(rooms.includes(data.roomname)){
+            io.to(socket.id).emit('room_valid',{success: false});
+        }
+        else{
+            rooms.push(data.roomname);
+            socket.join(data.roomname);
+            console.log("added room "+data.roomname);
+            io.to(socket.id).emit('room_valid',{success: true, myroom: data.roomname});
+            io.emit('room_added',{room : data.roomname});
+        }
+    });
+    socket.on("join_room",function(data) {
+        console.log(data.room,rooms[data.room],rooms);
+        if(rooms[data.room]==null){
+            io.to(socket.id).emit('join_room_valid',{success: false});
+            // console.log("Not this one");
+        }
+        else {
+            socket.join(data.roomname);
+            io.to(socket.id).emit('join_room_valid',{success: true, myroom: data.roomname});
+            io.to(data.roomname).emit('chat-msg',{user: socket.id,msg : "Joined the room"});
+        }
+    })
 });
 
+// io.sockets.on('connection',function (socket) {
+//     socket.on('create',function (room) {
+//         // console.log("room joined "+room);
+//         socket.join(room);
+//     })
+// })
--- public/working.js
+++ public/working.js
@@ -1,8 +1,11 @@
 var socket = io.connect('http://localhost:3999')
-
 var username = document.getElementById('username'),
     reg_btn = document.getElementById('register'),
-    room_error = document.getElementById("room_error");
+    room_error = document.getElementById("room_error"),
+    create_btn = document.getElementById("create_room_"),
+    roomname = document.getElementById("roomname");
+    join_btn = document.getElementById("room_join"),
+    join_name = document.getElementById("rooms");
 
 // emit eventss
 
@@ -17,6 +20,31 @@ reg_btn.addEventListener('click', function(){
   }
 });
 
+create_btn.addEventListener('click', function(){
+  let str=roomname.value;
+  str=str.trim();
+  if(str==""){
+    room_error.innerText="Room name must be non empty";
+  }
+  else{
+    // room_name = roomname.value;
+    socket.emit('new_room',{roomname: roomname.value});
+  }
+});
+
+join_btn.addEventListener('click',function() {
+  let str=join_name.value;
+  console.log(str);
+  str=str.trim();
+  if(str==""){
+    room_error.innerText="Room name must be non empty";
+  }
+  else{
+    socket.emit('join_room',{room: join_name.id});
+  }
+})
+
+
 socket.on('validation',function(data){
   if(data.success){
     let reg_wind = document.getElementById("register_window");
@@ -24,13 +52,65 @@ socket.on('validation',function(data){
     let create_room_wind = document.getElementById("create_room");
     let join_room_wind = document.getElementById("join_room");
     let name_disp = document.getElementById("name_display");
+    let room_list = document.getElementById("rooms");
+    let room_div = document.getElementById("room_list")
     name_disp.innerText=username.value;
     reg_wind.style.display = "none";
     profile_wind.style.display = "block";
     create_room_wind.style.display = "block";
     join_room_wind.style.display = "block";
+    room_div.style.display = "block";
+    for(room in data.rooms){
+      opt = document.createElement("option");
+      // btn.id = "join_room";
+      opt.innerText = data.rooms[room];
+      opt.id = room;
+      room_list.appendChild(opt);
+    }
   }
   else{
     room_error.innerText="Name already taken";
   }
 });
+
+socket.on('room_valid',function(data){
+  if(data.success){
+    room_name = data.myroom;
+    let reg_wind = document.getElementById("register_window");
+    let profile_wind = document.getElementById("user_prof");
+    let create_room_wind = document.getElementById("create_room");
+    let join_room_wind = document.getElementById("join_room");
+    let name_disp = document.getElementById("name_display");
+    let room_div = document.getElementById("room_list")
+    name_disp.innerText=username.value;
+    reg_wind.style.display = "none";
+    profile_wind.style.display = "block";
+    create_room_wind.style.display = "none";
+    join_room_wind.style.display = "block";
+    room_div.style.display = "none";
+    console.log("room created ")
+  }
+  else{
+    room_error.innerText="Name already taken";
+  }
+});
+
+socket.on('room_added',function(data) {
+  let room_list = document.getElementById("rooms");
+  opt = document.createElement("option");
+  // btn.id = "join_room";
+  opt.innerText = `Room : ${data.room}`;
+  room_list.appendChild(opt);
+})
+
+socket.on('chat-msg',function(data){
+  addmsg(data.user,data.msg);
+})
+
+function addmsg(user,msg) {
+  msg_div = document.getElementById("chat");
+  msg_elem = document.createElement("p");
+  package.innerText(`${user}: ${msg}`);
+  msg_div.appendChild(msg_elem);
+}
+// socket.emit('create','room1')
