31a83bba0234dfcccec1e294f52d21d037ae0541
Kartik Dhawaniya <200050060@iitb.ac.in>
2021-11-26 00:46:02 +0530

    Private rooms added

--- index.js
+++ index.js
@@ -40,10 +40,12 @@ const io = require('socket.io')(server, {
 // const io = socket(server);
 
 //data declaration
-players={};
-p_rooms={};
-rooms = [];
-count = {};
+players={};             //socket.id -> username map
+p_rooms={};             //socket.id -> room joined map
+public_rooms = [];      //list of all public rooms
+private_rooms = [];      //list of all private rooms
+count = {};             //room -> count of players in that room map
+room_keys = {};         //key of room if private , null otherwise
 
 var active = {};
 var init_time = {};
@@ -55,21 +57,28 @@ io.on('connection', function(socket){
     socket.on('new_player',function(data){
         if(Object.values(players).includes(data.username)){
             io.to(socket.id).emit('validation',{success: false});
+            // console.log("cvbinuo");
         }
         else{
             players[socket.id]=data.username;
             // console.log("added "+data.username);
-            io.to(socket.id).emit('validation',{success: true, rooms: rooms});
+            io.to(socket.id).emit('validation',{success: true, rooms: public_rooms});
         }
     });
     socket.on('new_room',function(data){
-        if(rooms.includes(data.roomname)){
+        if(public_rooms.includes(data.roomname) || private_rooms.includes(data.roomname)){
             io.to(socket.id).emit('room_valid',{success: false});
         }
         else{
-            rooms.push(data.roomname);
+            if(data.room_key===null)
+            {
+                public_rooms.push(data.roomname);
+            }else{
+                private_rooms.push(data.roomname);
+            }
             socket.join(data.roomname);
             count[data.roomname] = 1;
+            room_keys[data.roomname] = data.room_key;
             p_rooms[socket.id]=data.roomname;
             // console.log("added room "+data.roomname);
             io.to(socket.id).emit('room_valid',{success: true, myroom: data.roomname});
@@ -78,11 +87,20 @@ io.on('connection', function(socket){
         }
     });
     socket.on("join_room",function(data) {
-        if(!rooms.includes(data.room)){
-            io.to(socket.id).emit('room_valid',{success: false});
+        if(!public_rooms.includes(data.room) && !private_rooms.includes(data.room)){
+            io.to(socket.id).emit('room_valid',{success: false,msg: "no such room"});
             // console.log("Not this one");
         }
-        else {
+        else if(public_rooms.includes(data.room)){
+            socket.join(data.room);
+            count[data.room] += 1;
+            p_rooms[socket.id]=data.room;
+            io.to(socket.id).emit('room_valid',{success: true, user: socket.id,msg : "Joined the room"});
+            io.to(data.room).emit('chat-msg',{user: players[socket.id],msg : "Joined the room"});
+        }
+        else if(room_keys[data.room]!==data.key){
+            io.to(socket.id).emit('room_valid',{success: false,msg: "incorrect key"});
+        }else{
             socket.join(data.room);
             count[data.room] += 1;
             p_rooms[socket.id]=data.room;
@@ -108,17 +126,27 @@ io.on('connection', function(socket){
         socket.to(p_rooms[socket.id]).emit('chat-msg',{user: players[socket.id],msg : "Disconnected"});
         }
         count[p_rooms[socket.id]] -= 1;
-        if(count[p_rooms[socket.id]]==0) delete rooms[rooms.indexOf(p_rooms[socket.id])];
+        if(public_rooms.includes(p_rooms[socket.id])){
+            if(count[p_rooms[socket.id]]==0) delete public_rooms[public_rooms.indexOf(p_rooms[socket.id])];
+        }else if(private_rooms.includes(p_rooms[socket.id])){
+            if(count[p_rooms[socket.id]]==0) delete private_rooms[private_rooms.indexOf(p_rooms[socket.id])];
+        }
         refine_rooms();
         delete players[socket.id];
     })
 });
 
 function refine_rooms() {
-    for(i in rooms){
-        if(rooms[i]==null){
-            rooms[i]=rooms[i+1];
-            rooms[i+1]=null;
+    for(i in public_rooms){
+        if(public_rooms[i]==null){
+            public_rooms[i]=public_rooms[i+1];
+            public_rooms[i+1]=null;
+        }
+    }
+    for(i in private_rooms){
+        if(private_rooms[i]==null){
+            private_rooms[i]=private_rooms[i+1];
+            private_rooms[i+1]=null;
         }
     }
 }
